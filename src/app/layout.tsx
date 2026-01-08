959774dee5af90d84afdd1517864d4678e9f2a20
richazim <saibouazim90@gmail.com>
2026-01-08 02:55:04 +0100

    refactor:project structure refactored

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import './globals.css';
+
+import type { Metadata } from "next";
+import { Poppins } from 'next/font/google'
+
+import { appName } from "@/constants";
+
+const poppins = Poppins({
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    variable: '--font-poppins',
+})
+
+export const metadata: Metadata = {
+  title: `${appName}`,
+  description: `${appName} - The open-source storage solution you need.`,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable} font-poppins antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
